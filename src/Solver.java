940e2b9833edf304fd2c48aa77d08691280c1679
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-08-12 13:06:27 -0400

    created first solver function

--- src/Solver.java
+++ src/Solver.java
@@ -1,3 +1,22 @@
-public class Solver {
+import java.util.ArrayList;
 
+public class Solver {
+    private Grid grid;
+
+    public Solver(Grid g){
+        grid = g;
+    }
+
+    //given a box and a number, check if it can be determined where that number goes in that box
+    public int solveBoxNumber(int boxNum, int solveTarget){
+        ArrayList<Cell> curBox = grid.getBox(boxNum);
+        if (curBox.contains(solveTarget)){
+            return 0;
+        }
+        for (Cell cell: curBox){
+
+        }
+        System.out.println(grid);
+        return 0;
+    }
 }
